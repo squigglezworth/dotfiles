32a1ab83102ebf1bbc4b1d505b17a18a19f3ab75
squigglezworth <squigglezworth@gmail.com>
2022-08-23 00:39:01 -0400

    :rainbow: Updates to bash color utils

--- bash/scripts/color-utils.sh
+++ bash/scripts/color-utils.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+randNum() {
+    shuf -n 1 <(printf %s\\n {1..6} {9..14})
+}
+
 colorList() {
     for w in "$@"; do
-        printf '\e[38;5;%dm%s\e[0m ' "$((RANDOM%15+1))" "$w"
+        printf '\e[38;5;%dm%s\e[0m ' $(randNum) "$w"
     done
     printf '\033[1D'
 }
 
 randColor() {
-    printf '\e[38;5;%dm%s\e[0m' "$((RANDOM%15+1))" "$*"
+    printf '\e[38;5;%dm%s\e[0m' $(randNum) "$*"
+}
+
+rainbow() {
+    IFS=''
+    [[ "$@" =~ ${@//?/(.)} ]]
+    l=( "${BASH_REMATCH[@]:1}" )
+    for w in ${l[*]}; do
+        printf '\e[38;5;%dm%s\e[0m' $(randNum) "$w"
+    done
 }
--- bash/scripts/funs/tput-colors.sh
+++ bash/scripts/funs/tput-colors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for foo in {0..17..1} 
+for foo in {0..32..1} 
 do
 	tput setaf $foo
 	echo $foo

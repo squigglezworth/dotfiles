49b14fae8944c2a8a55a3ce43a4b35067fe19134
squigz <squigglezworth@gmail.com>
2022-09-02 04:12:25 -0400

    bash - :rainbow:

--- bash/scripts/color-utils.sh
+++ bash/scripts/color-utils.sh
@@ -23,3 +23,20 @@ rainbow() {
         printf '\e[38;5;%dm%s\e[0m' $(randNum) "$w"
     done
 }
+
+# $a=$1
+# shift
+# if [[ "$a" =~ ^line(s)?$ ]]; then
+#     printf '\e[38;5;%dm%s\e[0m' $(randNum) "$*"
+# else if [[ "$a" =~ ^word(s)?$ ]]; then
+#     for w in "$@"; do
+#         printf '\e[38;5;%dm%s\e[0m ' $(randNum) "$w"
+#     done
+# else if [[ "$a" =~ ^char(s)?$ ]]; then
+#     IFS=''
+#     [[ "$@" =~ ${@//?/(.)} ]]
+#     l=( "${BASH_REMATCH[@]:1}" )
+#     for w in ${l[*]}; do
+#         printf '\e[38;5;%dm%s\e[0m' $(randNum) "$w"
+#     done
+# fi

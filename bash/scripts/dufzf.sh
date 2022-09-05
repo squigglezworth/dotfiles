34a43a4b4d96549f0c6f02b293bebb7b0037ffc1
squigglezworth <squigglezworth@gmail.com>
2022-09-05 04:43:33 -0400

    bash - Add dufzf script

---
+++ bash/scripts/dufzf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while [ 1 ]; do
+    # Build a list of files/directories
+    IFS=$'\n' f=('Move up one dir ..' $(cdu -risdhD "."/*))
+    
+    # Present the list to the user, store their selection for later
+    n=$(echo "${f[*]}" | fzf --ansi --preview='cd {3..}; cdu -risdhD ./*' --reverse)
+    
+    # Required to Ctrl-C without spamming it
+    test $? -eq 130 && break
+
+    # Finally, navigate to the user's selection, setting the PWD for the next iteration
+    cd $(echo "$n" | awk '{print $NF}')
+done

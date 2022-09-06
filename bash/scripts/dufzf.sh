010e28c4a43a963834b6445ce9cecf2719d44c9b
squigglezworth <squigglezworth@gmail.com>
2022-09-05 22:09:19 -0400

    bash - WIP fzf-powered disk-usage script

--- bash/scripts/dufzf.sh
+++ bash/scripts/dufzf.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# TODO cleaner way of going up a dir, hopefully with a keybind
-
 while [ 1 ]; do
+    echo -ne "Loading...\r"
+
     # Build a list of files/directories
-    IFS=$'\n' f=('Move up one dir ..' $(cdu -risdhD "."/*))
+    IFS=$'\n' f=($(du -sh | awk '{print $1 "\t" Total}') $(echo -en "Move up one dir\t..") $(cdu -sidhD * | tac))
     
     # Present the list to the user, store their selection for later
-    n=$(echo "${f[*]}" | fzf --ansi --preview='cd {3..}; cdu -risdhD ./*' --reverse)
+    n=$(echo -n "${f[*]}" | fzf \
+        --ansi \
+        --header-lines=1 \
+        --color="header:bright-white" \
+        --preview='cd {3..} && cdu -sidhD * | tac') \
+        # --bind='bs:preview(echo "Loading...")+reload(cd .. && cdu -sidhD * | tac)') \
     
     # Required to Ctrl-C without spamming it
-    test $? -eq 130 && break
+    [ $? -eq 130 ] && break
 
     # Finally, navigate to the user's selection, setting the PWD for the next iteration
-    cd $(echo "$n" | awk '{print $NF}')
+    f=$(echo "$n" | awk '{print $NF}')
+    [ "$f" == ".." ] && cd .. && continue
+    [ -d "$f" ] && cd "$f"
 done

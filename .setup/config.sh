4dd4dc124ec747e8aeedc86844bfd8e749c3ffea
squigglezworth <squigglezworth@gmail.com>
2022-09-07 22:04:21 -0400

    config.sh - Tweaks from first live test
    - Remove trailing slash when calling `rm`, to prevent accidentally removing base content
    - Link rofi config
    - Link music dir
    - Potential fix for initial mpd setup
    - Temporarily disable mpdscribble setup
    - Mount with sudo

--- .setup/config.sh
+++ .setup/config.sh
@@ -20,11 +20,11 @@ echo -ne "\t\t Configuring mounts ... "
 sudo systemctl daemon-reload
 sudo mkdir -p /mnt/disk-{1,2,3} /mnt/usb{,2} /mnt/misc
 sudo chown -R squigz:squigz /mnt/
-for i in disk-{1,2,3} usb; do mount /mnt/$i; done
+for i in disk-{1,2,3} usb; do sudo mount /mnt/$i; done
 echoDone
 
 echo -ne "\t\t Configuring tops (top, atop, htop) ... "
-rm -rf $HOME/.config/procps/ $HOME/.config/htop/ $HOME/.atoprc
+rm -rf $HOME/.config/procps $HOME/.config/htop $HOME/.atoprc
 mkdir -p $HOME/.config/procps/ $HOME/.config/htop/
 ln -fs $PWD/tops/toprc $HOME/.config/procps/
 ln -fs $PWD/tops/htoprc $HOME/.config/htop/
@@ -44,19 +44,19 @@ ln -fs $PWD/bash/bash_profile $HOME/.bash_profile
 echoDone
 
 echo -ne "\t\t Copying fonts ... "
-rm -rf $HOME/.fonts/
+mkdir -p $HOME/.fonts
 gpg --decrypt $PWD/fonts/fonts.tar.gz.gpg > /tmp/fonts.tar.gz
 tar -xaf /tmp/fonts.tar.gz -C $HOME/.fonts/
 echoDone
 
 echo -ne "\t\t Copying icons ... "
-rm -rf $HOME/.icons/
+mkdir -p $HOME/.icons
 gpg --decrypt $PWD/icons/icons.tar.gz.gpg > /tmp/icons.tar.gz
 tar -xaf /tmp/icons.tar.gz -C $HOME/.icons/
 echoDone
 
 echo -ne "\t\t Configuring gtk ... "
-rm -rf $HOME/.config/gtk-3.0/
+rm -rf $HOME/.config/gtk-3.0
 ln -fs $PWD/gtk/gtkrc-2.0 $HOME/.gtkrc-2.0
 ln -fs $PWD/gtk/gtk-3.0/ $HOME/.config/
 ln -fs $PWD/mimeapps.list $HOME/.config/
@@ -68,52 +68,56 @@ ln -fs $PWD/X/xinitrc $HOME/.xinitrc
 echoDone
 
 echo -ne "\t\t Configuring i3 ... "
-rm -rf $HOME/.i3/
+rm -rf $HOME/.i3
 ln -fs $PWD/i3/ $HOME/.i3; echoDone
 
 echo -ne "\t\t Configuring polybar ... "
-rm -rf $HOME/.config/polybar/
+rm -rf $HOME/.config/polybar
 ln -fs $PWD/polybar/ $HOME/.config/; echoDone
 
 echo -ne "\t\t Configuring wallpaper ... "
 ln -fs $PWD/feh $HOME/.fehbg; echoDone
 
 echo -ne "\t\t Configuring dunst ... "
-rm -rf $HOME/.config/dunst/
+rm -rf $HOME/.config/dunst
 ln -fs $PWD/dunst/ $HOME/.config/; echoDone
 
 echo -ne "\t\t Configuring vim ... "
-rm -rf $HOME/.vim/ $HOME/.vimrc
+rm -rf $HOME/.vim $HOME/.vimrc
 ln -fs $PWD/vim/vim/ $HOME/.vim
 ln -fs $PWD/vim/vimrc $HOME/.vimrc
 echoDone
 
 echo -ne "\t\t Configuring sublime ... "
-rm -rf $HOME/.config/sublime-text/
+rm -rf $HOME/.config/sublime-text
 cp $PWD/sublime-text/Local/session.default $PWD/sublime-text/Local/Session.sublime_session
 ln -fs $PWD/sublime-text/ $HOME/.config/; echoDone
 
+echo -ne "\t\t Configuring rofi ... "
+rm -rf $HOME/.config/rofi
+ln -fs $PWD/rofi/ $HOME/.config/; echoDone
+
 echo -ne "\t\t Configuring rofi-blezz ... "
-rm -rf $HOME/.config/blezz/
+rm -rf $HOME/.config/blezz
 ln -fs $PWD/rofi-blezz/ $HOME/.config/; echoDone
 
 echo -ne "\t\t Configuring ranger ... "
-rm -rf $HOME/.config/ranger/
+rm -rf $HOME/.config/ranger
 ln -fs $PWD/ranger/ $HOME/.config/; echoDone
 
 echo -ne "\t\t Configuring parcellite ... "
-rm -rf $HOME/.config/parcellite/
+rm -rf $HOME/.config/parcellite
 ln -fs $PWD/parcellite/ $HOME/.config/; echoDone
 
 echo -ne "\t\t Configuring pulsemixer ... "
 ln -fs $PWD/pulsemixer/pulsemixer.cfg $HOME/.config/; echoDone
 
 echo -ne "\t\t Configuring PulseEffects ... "
-rm -rf $HOME/.config/PulseEffects/
+rm -rf $HOME/.config/PulseEffects
 ln -fs $PWD/PulseEffects/ $HOME/.config/; echoDone
 
 echo -ne "\t\t Configuring conky ... "
-rm  -rf $HOME/.conky/
+rm  -rf $HOME/.conky
 ln -fs $PWD/conky/ $HOME/.conky; echoDone
 
 echo -ne "\t\t Configuring picom ... "
@@ -132,18 +136,22 @@ ln -fs $PWD/user-dirs.dirs $HOME/.config/; echoDone
 ##  ██  ██  ██ ██    ██      ██ ██ ██
 ##  ██      ██  ██████  ███████ ██  ██████
 
-echo -e "\t Configuring music software:"
+echo -e "\t Configuring music stuff:"
+
+echo -ne "\t\t Linking music dir ... "
+ln -fs /mnt/disk-1/media/music/ $HOME/; echoDone
+
 echo -ne "\t\t Configuring ncmpcpp ... "
-rm -rf $HOME/.config/ncmpcpp/
+rm -rf $HOME/.config/ncmpcpp
 ln -fs $PWD/ncmpcpp/ $HOME/.config/; echoDone
 
 echo -ne "\t\t Configuring beets ... "
-rm -rf $HOME/.config/beets/
+rm -rf $HOME/.config/beets
 ln -fs $PWD/beets/ $HOME/.config/; echoDone
 
 echo -ne "\t\t Configuring mpd ... "
-sudo systemctl disable --now mpd &>>/tmp/dotfiles.log
-rm -rf $HOME/.config/mpd/
+sudo systemctl disable --now mpd mpd.socket &>>/tmp/dotfiles.log
+rm -rf $HOME/.config/mpd
 ln -fs $PWD/mpd/ $HOME/.config/
 systemctl --user enable --now mpd &>>/tmp/dotfiles.log
 mpc update &>>/tmp/dotfiles.log
@@ -154,5 +162,5 @@ mkdir -p $HOME/.mpdscribble
 sudo cat /etc/mpdscribble.conf | tee $HOME/.mpdscribble/mpdscribble.conf >/dev/null
 sed -i "s/^username \=/username = $LASTFM_USERNAME/" $HOME/.mpdscribble/mpdscribble.conf
 sed -i "s/^password \=/password = $LASTFM_PASSWORD/" $HOME/.mpdscribble/mpdscribble.conf
-systemctl --user enable --now mpdscribble &>>/tmp/dotfiles.log
+# systemctl --user enable --now mpdscribble &>>/tmp/dotfiles.log
 echoDone
